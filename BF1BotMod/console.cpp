26cf3e71792f9023e904224f11f1895718da311e
DoDoCat <dell20091234@gmail.com>
2024-04-06 22:27:33 +0800

    Update the project

--- BF1BotMod/console.cpp
+++ BF1BotMod/console.cpp
@@ -6,5 +6,7 @@ std::unique_ptr<BFConsole> m_pConsole = std::make_unique<BFConsole>();
 
 void BFConsole::Run()
 {
+	//Create a bf1 console so that we can input some useful commands
 	ImGui::Begin(WindowName, FALSE, 0);
+	ImGui::End();
 }
