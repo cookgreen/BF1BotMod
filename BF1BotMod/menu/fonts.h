09929d57c5cba6eefb3f3131a65a8308e7e9cade
DoDoCat <dell20091234@gmail.com>
2025-02-10 21:10:18 +0800

    Clearup codebase

--- BF1BotMod/menu/fonts.h
+++
@@ -1,8 +0,0 @@
-class CImGuiFonts
-{
-public:
-	void LoadFonts();
-	ImFont* font_Main;
-	ImFont* font_Credits;
-};
-extern std::unique_ptr<CImGuiFonts>m_pMenuFonts;
--- BF1BotMod/menu/menu.h
+++
@@ -1,6 +0,0 @@
-class CMenu
-{
-public:
-	void Run();
-};
-extern std::unique_ptr<CMenu>m_pMenu;
